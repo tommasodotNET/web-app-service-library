9d4edc16aadbe9ee319dae5e3ed2c2a86ed6435d
Tommaso Stocchi <tstocchi@alteanet.it>
2020-05-26 17:25:10 +0200

    Added service configuration

--- WebAppServiceLibrary/Providers/ServiceProviderConfiguration.cs
+++ WebAppServiceLibrary/Providers/ServiceProviderConfiguration.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using WebAppServiceLibrary.Extensions;
 
 namespace WebAppServiceLibrary.Providers
 {
-   public class ServiceProviderConfiguration
+   public static class ServiceProviderConfiguration
    {
+      public static IServiceCollection ConfigureService(this IServiceCollection services, IConfiguration configuration)
+      {
+         Console.WriteLine("Configuring services...");
+
+         services.AddServicesWithReflection()
+             .ConfigureOptions(configuration)
+             .AddDbContextWithReflection(configuration)
+             .BuildServiceProvider();
+
+         Console.WriteLine("Done.");
+
+         return services;
+      }
    }
 }
