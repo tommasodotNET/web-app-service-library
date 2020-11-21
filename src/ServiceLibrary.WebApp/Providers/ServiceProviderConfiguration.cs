1cdd3e93b267c8e192b780cb000e5d2d35ad2c02
Tommaso Stocchi <tommasostocchi@outlook.com>
2020-11-21 19:19:53 +0100

    Test +semver: patch

--- src/ServiceLibrary.WebApp/Providers/ServiceProviderConfiguration.cs
+++ src/ServiceLibrary.WebApp/Providers/ServiceProviderConfiguration.cs
@@ -9,8 +9,7 @@ namespace ServiceLibrary.WebApp.Providers
 {
    public static class ServiceProviderConfiguration
    {
-      public static IServiceCollection ConfigureService(this IServiceCollection services, 
-         IConfiguration configuration)
+      public static IServiceCollection ConfigureService(this IServiceCollection services, IConfiguration configuration)
       {
          Console.WriteLine("Configuring services...");
 
